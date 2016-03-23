b3a8c368534f41558e2811b28f6eecdc5bc0bf87
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2016-03-23 18:15:42 +0100

    Add e01-doopz
    Get file structure with MD5 completed

---
+++ chapter02-node-programming-fundamentals/e01-doopz/README.md
@@ -0,0 +1,10 @@
+# Chapter 1: Getting started &mdash; e01-doopz
+> find duplicate files recursively
+
+## Description
+A command-line application that looks for duplicate files recursively, from a given directory.
+
+The application displays:
++ files that have the same name
++ files that have the same length
++ files that have the same MD5 checksum
---
+++ chapter02-node-programming-fundamentals/e01-doopz/public/src/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var log4js = require("log4js");
+var logger = log4js.getLogger("main");
+logger.setLevel("DEBUG");
+
+var fs = require("fs");
+var path = require("path");
+var async = require("async");
+var crypto = require("crypto");
+
+
+var files = [];
+
+function getFilenames(fromDir, cb) {
+  fs.readdir(fromDir, function (err, filenames) {
+    async.map(filenames, function (filename, next) {
+      let filePath = path.join(fromDir, filename);
+      fs.stat(filePath, function (err, stats) {
+        if (stats.isFile()) {
+          let stream = fs.createReadStream(filePath);
+          let md5Hash = crypto.createHash("md5");
+          stream.on("data", function (data) {
+            md5Hash.update(data, "utf8");
+          });
+
+          stream.on("end", function () {
+            let md5 = md5Hash.digest("hex");
+            files.push({ name: filename, path: filePath, size: stats.size, md5: md5 });
+            next();
+          });
+
+        } else if (stats.isDirectory()) {
+          getFilenames(filePath, next);
+        }
+      });
+    }, function (err) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, files);
+    });
+  });
+}
+
+getFilenames("/tmp/test", function done(err, files) {
+  if (err) {
+    throw err;
+  }
+  console.log(files);
+});
