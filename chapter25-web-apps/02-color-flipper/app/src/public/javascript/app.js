287a7a5ac7b1ee51615c734b7d9fb65dd2a08f74
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2020-06-20 20:05:11 +0200

    Add css + js for simple mode

--- chapter25-web-apps/02-color-flipper/app/src/public/javascript/app.js
+++ chapter25-web-apps/02-color-flipper/app/src/public/javascript/app.js
@@ -2,39 +2,13 @@
 
 const nonNavArea=document.querySelector('#nonNavArea');
 let currentColorIndex = -1;
-const colors = [ 'violet', 'green', 'red', 'blue' ];
+const colors = [ '#f15025', 'green', 'red', 'rgba(133, 122, 200)', 'hsl(205, 78%, 60%)' ];
 const colorName = document.querySelector('#colorName');
-const btn = document.querySelector('button');
+const btn = document.querySelector('#changeColorBtn');
+
 
 btn.addEventListener('click', () => {
   currentColorIndex = (currentColorIndex + 1) % colors.length;
   nonNavArea.style.background = colors[currentColorIndex];
   colorName.textContent = colors[currentColorIndex];
 });
-
-
-
-/* prev work
-window.addEventListener('click', evt => {
-  const dot = document.createElement('div');
-  dot.className = 'dot';
-  dot.style.left = `${ evt.pageX - 4 }px`;
-  dot.style.top = `${ evt.pageY - 4 }px`;
-  document.body.appendChild(dot);
-});
-
-window.addEventListener('keydown', evt => {
-  console.log('keydown');
-  if (evt.key == 'v') {
-    document.body.style.background = 'violet';
-  }
-});
-
-window.addEventListener('keyup', evt => {
-  console.log('keyup');
-  if (evt.key == 'v') {
-    document.body.style.background = '';
-  }
-});
-
-*/
