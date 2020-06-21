663058a92ced45b422d4e69204aee241f0db20c4
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2020-06-21 19:23:24 +0200

    Add more examples

--- chapter25-web-apps/01-bootstrap-refresher/app/src/index.js
+++ chapter25-web-apps/01-bootstrap-refresher/app/src/index.js
@@ -11,8 +11,19 @@ const helmet = require('helmet');
 const compression = require('compression');
 const requestLogger = require('morgan');
 
+/*
+  Routes
+*/
+const routes = require('./routes/error-routes');
+
 
 const app = express();
+
+/*
+  View engine setup
+*/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
 /* 3rd party middleware */
@@ -23,7 +34,12 @@ app.use(helmet());
 app.use(compression());
 app.use(express.static(path.join(__dirname, process.env['PUBLIC_STATIC_RESOURCES_PATH'] || 'public')));
 
+/*
+  Error Handling Routes
+*/
+app.use(routes.notFound);
+app.use(routes.error);
 
 logger.info(`Express application started with env=${ app.get('env')}; NODE_ENV=${ process.env['NODE_ENV'] }`);
-
+logger.info(`View Caching: ${ app.get('view cache') }`);
 module.exports = app;
